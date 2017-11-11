32f1df2b5f9abc6ab303a779e5c180d316bf641e
jgtaylor <sonicbphuct@gmail.com>
2017-11-11 20:30:05 +0100

    1st try

--- index.js
+++ index.js
@@ -100,63 +100,57 @@ function configGen( config ) {
 }
 
 function button( d, cmd ) {
-	if ( d.pin.getMode() !== "output" ) {
-		d.pin.mode( "output" );
+	if ( bone.getPinMode( d.pin )
+		.gpio.direction !== "out" ) {
+		bone.pinMode( d.pin, bone.OUTPUT, ( d.pin ) => {
+			console.log( `Setting ${d.pin} to Output` )
+		} );
 	}
 	switch ( cmd ) {
-	case "on":
-	{
-		bone.digitalWrite( d.pin, 1 );
-		let retMsg = [ "state", {
-			device: d.id,
-			mode: bone.getPinMode( d.pin )
-				.gpio,
-			value: d.pin.read()
-		} ];
-		WebSock.send( JSON.stringify( retMsg ) );
-		break;
-	}
-	case "off":
-	{
-		bone.digitalWrite( d.pin, 0 );
-		let retMsg = [ "state", {
-			device: d.id,
-			mode: d.pin.getMode(),
-			value: d.pin.read()
-		} ];
-		WebSock.send( JSON.stringify( retMsg ) );
-		break;
-	}
-	case "getState":
-	{
-		WebSock.send( JSON.stringify( [ "state", {
-			device: d.id,
-			mode: d.pin.getMode(),
-			value: d.pin.read()
-		} ] ) );
-		break;
-	}
-	default:
-	{
-		break;
-	}
+		case "on":
+			{
+				bone.digitalWrite( d.pin, 1 );
+				button( d, "getState" );
+				break;
+			}
+		case "off":
+			{
+				bone.digitalWrite( d.pin, 0 );
+				button( d, "getState" );
+				break;
+			}
+		case "getState":
+			{
+				let retMsg = [ "state", {
+					device: d.id,
+					mode: bone.getPinMode( d.pin )
+						.gpio,
+					value: bone.digitialRead( d.pin )
+				} ];
+				WebSock.send( JSON.stringify( retMsg ) );
+				break;
+			}
+		default:
+			{
+				break;
+			}
 	}
 }
 
 function dimmer( d, cmd ) {
 	switch ( cmd ) {
-	case "read":
-		{
-			WebSock.send( JSON.stringify( [ "reading", {
-				device: d.device,
-				value: analogRead()
-			} ] ) );
-		}
-		break;
-	case "expression":
+		case "read":
+			{
+				WebSock.send( JSON.stringify( [ "reading", {
+					device: d.device,
+					value: analogRead()
+				} ] ) );
+			}
+			break;
+		case "expression":
 
-		break;
-	default:
+			break;
+		default:
 
 	}
 	//analogRead();
@@ -179,37 +173,37 @@ function msgParse( msg ) {
 		}
 	};
 	switch ( m[ 0 ] ) {
-	case "cmd":
-	{
-		let d = device( configMap );
-		switch ( d.type ) {
-		case "button":
-		{
-			button( d, m[ 1 ].cmd );
-			break;
-		}
-		case "virtual":
-		{
-			virtual( d, m[ 1 ].cmd );
-			break;
-		}
-		case "dimmer":
-		{
-			dimmer( d, m[ 1 ].cmd );
-			break;
-		}
+		case "cmd":
+			{
+				let d = device( configMap );
+				switch ( d.type ) {
+					case "button":
+						{
+							button( d, m[ 1 ].cmd );
+							break;
+						}
+					case "virtual":
+						{
+							virtual( d, m[ 1 ].cmd );
+							break;
+						}
+					case "dimmer":
+						{
+							dimmer( d, m[ 1 ].cmd );
+							break;
+						}
+					default:
+						break;
+				}
+				break;
+			}
+		case "config":
+			{
+				WebSock.send( JSON.stringify( [ "config", configGen( configMap ) ] ) );
+				break;
+			}
 		default:
 			break;
-		}
-		break;
-	}
-	case "config":
-	{
-		WebSock.send( JSON.stringify( [ "config", configGen( configMap ) ] ) );
-		break;
-	}
-	default:
-		break;
 
 	}
 }
@@ -239,5 +233,10 @@ function WebSockconnect( state ) {
 	} );
 }
 
-WebSockconnect( 0 );
+configMap.forEach( ( d ) => {
+	bone.pinMode( d.pin, bone.OUTPUT, ( d.pin ) => {
+		console.log( `Setting ${d.pin} to Output` )
+	} );
+} );
 
+WebSockconnect( 0 );
